f91c73406a5958475ef66e50a30da100c22fc178
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2024-01-17 14:22:14 +0700

    setup tailwind and css

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      'purple': '#4126b2',
+      'gray-dark': '#15171a',
+      'gray': '#979797',
+      'gray-light': '#979797',
+      'gray-lighter': '#fcfcfc',
+      'white': '#ffffff',
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    }
+    // extend: {
+    //   backgroundImage: {
+    //     'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+    //     'gradient-conic':
+    //       'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    //   },
+    // },
+  },
+  plugins: [],
+}
+
