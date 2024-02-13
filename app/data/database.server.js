4629463fb316a7b87b5bb9e30104f7a7617df74b
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2024-02-13 22:11:35 +0700

    set prisma and prisma client same versions

--- app/data/database.server.js
+++ app/data/database.server.js
@@ -1,6 +1,6 @@
 // import { PrismaClient as PrismaClientEdge  } from '@prisma/client/edge';
 import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate'
+// import { withAccelerate } from '@prisma/extension-accelerate'
 
 /**
  * @type PrismaClient
