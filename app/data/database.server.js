071948615f06e52bf6db75ca8d092d7bf8f57d69
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2024-02-13 22:03:51 +0700

    enable prisma accelerate

--- app/data/database.server.js
+++ app/data/database.server.js
@@ -1,18 +1,20 @@
-// import { PrismaClient as PrismaClientEdge  } from '@prisma/client/edge';
+
+
+import { PrismaClient as PrismaClientEdge  } from '@prisma/client/edge';
 import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 /**
  * @type PrismaClient
  */
-const prisma = new PrismaClient();
-// let prisma = null;
-// if (typeof EdgeRuntime !== 'string') {
-//     prisma = new PrismaClientEdge()
-// } else {
-//     prisma = new PrismaClient()
-// }
-// prisma = prisma.$extends(withAccelerate())
+// const prisma = new PrismaClient();
+let prisma = null;
+if (typeof EdgeRuntime !== 'string') {
+    prisma = new PrismaClientEdge()
+} else {
+    prisma = new PrismaClient()
+}
+prisma = prisma.$extends(withAccelerate())
 prisma.$connect();
 
 export { prisma };
